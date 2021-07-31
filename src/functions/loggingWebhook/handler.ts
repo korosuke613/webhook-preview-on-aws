da1db569e861e20bf9dfa0744c211c462ff502c7
Futa HIRAKOBA <futa-hirakoba@cybozu.co.jp>
2021-07-31 12:42:43 +0900

    feat: extract header and body only

--- src/functions/loggingWebhook/handler.ts
+++ src/functions/loggingWebhook/handler.ts
@@ -2,7 +2,10 @@ import 'source-map-support/register';
 import {APIGatewayProxyEvent, Handler} from "aws-lambda";
 
 const loggingWebhook: Handler<APIGatewayProxyEvent> = async (event) => {
-  console.log(JSON.stringify(event, null, 2))
+  const headers = event.multiValueHeaders
+  const body = JSON.parse(event.body)
+
+  console.log(JSON.stringify({headers, body}, null, 2))
 }
 
 export const main: Handler<APIGatewayProxyEvent> = loggingWebhook;
